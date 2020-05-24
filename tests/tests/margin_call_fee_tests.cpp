c514195196a0009def8bfe356b49db27cdb1e512
Michel Santos <MichelSantos@users.noreply.github.com>
2020-05-24 10:40:28 -0400

    BSIP74: Test of prices and ratios related to MCFR

--- tests/tests/margin_call_fee_tests.cpp
+++ tests/tests/margin_call_fee_tests.cpp
@@ -99,9 +99,9 @@ struct bitasset_database_fixture : database_fixture {
 BOOST_FIXTURE_TEST_SUITE(margin_call_fee_tests, bitasset_database_fixture)
 
    /**
-    * Test calculating the margin fee from filled_debt, the reference price, and fraction divisor
+    * Test the effects of different MCFRs on derived prices and ratios
     */
-   BOOST_AUTO_TEST_CASE(calculate_margin_fee_test) {
+   BOOST_AUTO_TEST_CASE(mcfr_tests) {
       try {
          ACTORS((charlie))
          const asset_id_type core_id;
@@ -114,62 +114,163 @@ BOOST_FIXTURE_TEST_SUITE(margin_call_fee_tests, bitasset_database_fixture)
          //////
          // Initialize
          //////
-         asset filled_debt;
-         asset margin_fee_collateral;
-         price match_price;
-         uint16_t mcfr;
+         fc::optional<uint16_t> mcfr;
+         price expected_offer_price; // The price offered by the margin call
+         price expected_paid_price; // The effective price paid by the margin call
+         ratio_type expected_margin_call_pays_ratio;
 
          const asset_object core = core_id(db);
          const uint16_t smartbit_market_fee_percent = 2 * GRAPHENE_1_PERCENT;
          create_bitasset("SMARTBIT2", charlie.id, smartbit_market_fee_percent, charge_market_fee, 2); //, core_id, GRAPHENE_MAX_SHARE_SUPPLY, {}, smartbit_margin_call_fee_ratio);
          generate_block();
          const asset_object smartbit2 = get_asset("SMARTBIT2");
-         // const asset_id_type smartbit2_id = smartbit2.id;
          BOOST_CHECK_EQUAL(2, smartbit2.precision);
-         const int64_t smartbit2_unit
-                 = asset::scaled_precision(smartbit2.precision).value; // 100 satoshi SMARTBIT2 in 1 SMARTBIT2
+
+         // Construct a price feed
+         // Initial price of 1 satoshi SMARTBIT2 for 20 satoshi CORE
+         // = 0.0001 SMARTBIT2 for 0.00020 CORE = 1 SMARTBIT2 for 2 CORE
+         const price initial_price =
+                 smartbit2.amount(1) / core.amount(20); // 1 satoshi SMARTBIT2 for 20 satoshi CORE
+
+         price_feed feed;
+         feed.settlement_price = initial_price;
+         feed.maintenance_collateral_ratio = 1750; // MCR of 1.75x
+         feed.maximum_short_squeeze_ratio = 1500; // MSSR of 1.50x
 
          //////
-         // Check the fee when MCFR equals 0%
+         // Check the fee when MSSR = 150% and MCFR is not set
          //////
-         filled_debt = smartbit2.amount(100 * smartbit2_unit);
-         // Price of 1 satoshi SMARTBIT2 for 20 satoshi Core
-         // -> 0.01 SMARTBIT2 for 0.00020 Core = 100 SMARTBIT2 for 2 Core = 50 SMARTBIT2 for 1 Core
-         match_price = smartbit2.amount(1) / core.amount(20);
+         mcfr = {};
+
+         // Expected paid price = price / MSSR
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (1500 / 1000)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (3 / 2)
+         // = (1 satoshi SMARTBIT2 / 30 satoshi Core)
+         expected_paid_price = price(smartbit2.amount(1), core.amount(30));
+         BOOST_CHECK(expected_paid_price == feed.max_short_squeeze_price());
+
+         // Expected offer price = price / (MSSR-MCFR)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / ([1500 - 0] / 1000)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (3 / 2)
+         // = (1 satoshi SMARTBIT2 / 30 satoshi Core)
+         expected_paid_price = price(smartbit2.amount(1), core.amount(30));
+         BOOST_CHECK(expected_paid_price == feed.margin_call_order_price(mcfr));
+
+         // Expected margin call pays ratio = (MSSR-MCFR) / MSSR
+         // = (1500 - 0) / 1500
+         // = 1
+         expected_margin_call_pays_ratio = ratio_type(1, 1);
+         BOOST_CHECK(expected_margin_call_pays_ratio == feed.margin_call_pays_ratio(mcfr));
+
+
+         //////
+         // Check the fee when MSSR = 150% and MCFR = 0
+         //////
          mcfr = 0;
-         margin_fee_collateral = graphene::chain::detail::calculate_collateral(filled_debt, mcfr, match_price);
 
-         BOOST_CHECK_EQUAL(0, margin_fee_collateral.amount.value);
-         BOOST_CHECK(core_id == margin_fee_collateral.asset_id);
+         // Expected paid price = price / MSSR
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (1500 / 1000)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (3 / 2)
+         // = (1 satoshi SMARTBIT2 / 30 satoshi Core)
+         expected_paid_price = price(smartbit2.amount(1), core.amount(30));
+         BOOST_CHECK(expected_paid_price == feed.max_short_squeeze_price());
+
+         // Expected offer price = price / (MSSR-MCFR)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / ([1500 - 0] / 1000)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (3 / 2)
+         // = (1 satoshi SMARTBIT2 / 30 satoshi Core)
+         expected_paid_price = price(smartbit2.amount(1), core.amount(30));
+         BOOST_CHECK(expected_paid_price == feed.margin_call_order_price(mcfr));
+
+         // Expected margin call pays ratio = (MSSR-MCFR) / MSSR
+         // = (1500 - 0) / 1500
+         // = 1
+         expected_margin_call_pays_ratio = ratio_type(1, 1);
+         BOOST_CHECK(expected_margin_call_pays_ratio == feed.margin_call_pays_ratio(mcfr));
 
 
          //////
-         // Check the fee when MCFR equals 5%
+         // Check the fee when MSSR = 150% and MCFR = 5%
          //////
-         filled_debt = smartbit2.amount(100 * smartbit2_unit);
-         // Price of 1 satoshi SMARTBIT2 for 20 satoshi Core
-         // -> 0.01 SMARTBIT2 for 0.00020 Core = 100 SMARTBIT2 for 2 Core = 50 SMARTBIT2 for 1 Core
-         match_price = smartbit2.amount(1) / core.amount(20);
-         mcfr = 50;
-         margin_fee_collateral = graphene::chain::detail::calculate_collateral(filled_debt, mcfr, match_price);
+         mcfr = 50; // 5% expressed in terms of GRAPHENE_COLLATERAL_RATIO_DENOM
 
-         // 100 SMARTBIT2 / (50 SMARTBIT2 / 1 Core) * 5% = 2 Core * 5% = 0.1 Core = 10000 satoshi Core
-         BOOST_CHECK_EQUAL(10000, margin_fee_collateral.amount.value);
-         BOOST_CHECK(core_id == margin_fee_collateral.asset_id);
+         // Expected paid price = price / MSSR
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (1500 / 1000)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (3 / 2)
+         // = (1 satoshi SMARTBIT2 / 30 satoshi Core)
+         expected_paid_price = price(smartbit2.amount(1), core.amount(30));
+         BOOST_CHECK(expected_paid_price == feed.max_short_squeeze_price());
+
+         // Expected offer price = price / (MSSR-MCFR)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / ([1500 - 50] / 1000)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (1450 / 1000)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (145 / 100)
+         // = (1 satoshi SMARTBIT2 / 2 satoshi Core) / (145 / 10)
+         // = (10 satoshi SMARTBIT2 / 290 satoshi Core)
+         // = (1 satoshi SMARTBIT2 / 29 satoshi Core)
+         expected_paid_price = price(smartbit2.amount(1), core.amount(29));
+         BOOST_CHECK(expected_paid_price == feed.margin_call_order_price(mcfr));
+
+         // Expected margin call pays ratio = (MSSR-MCFR) / MSSR
+         // = (1500 - 50) / 1500
+         // = 1450 / 1500 = 145 / 150 = 29 / 30
+         expected_margin_call_pays_ratio = ratio_type(29, 30);
+         BOOST_CHECK(expected_margin_call_pays_ratio == feed.margin_call_pays_ratio(mcfr));
 
 
          //////
-         // Check the fee when MCFR equals 5% when the price is defined in an inverted form
+         // Check the fee when MSSR = 150% and MCFR = 30%
          //////
-         filled_debt = smartbit2.amount(100 * smartbit2_unit);
-         // Price of 1 satoshi SMARTBIT2 for 20 satoshi Core
-         match_price = core.amount(20) / smartbit2.amount(1); // Inverted price
-         mcfr = 50;
-         margin_fee_collateral = graphene::chain::detail::calculate_collateral(filled_debt, mcfr, match_price);
+         mcfr = 300; // 30% expressed in terms of GRAPHENE_COLLATERAL_RATIO_DENOM
 
-         // 100 SMARTBIT2 / (50 SMARTBIT2 / 1 Core) * 5% = 2 Core * 5% = 0.1 Core = 10000 satoshi Core
-         BOOST_CHECK_EQUAL(10000, margin_fee_collateral.amount.value);
-         BOOST_CHECK(core_id == margin_fee_collateral.asset_id);
+         // Expected paid price = price / MSSR
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (1500 / 1000)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (3 / 2)
+         // = (1 satoshi SMARTBIT2 / 30 satoshi Core)
+         expected_paid_price = price(smartbit2.amount(1), core.amount(30));
+         BOOST_CHECK(expected_paid_price == feed.max_short_squeeze_price());
+
+         // Expected offer price = price / (MSSR-MCFR)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / ([1500 - 300] / 1000)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (1200 / 1000)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (6 / 5)
+         // = (5 satoshi SMARTBIT2 / 120 satoshi Core)
+         // = (1 satoshi SMARTBIT2 / 24 satoshi Core)
+         expected_paid_price = price(smartbit2.amount(1), core.amount(24));
+         BOOST_CHECK(expected_paid_price == feed.margin_call_order_price(mcfr));
+
+         // Expected margin call pays ratio = (MSSR-MCFR) / MSSR
+         // = (1500 - 300) / 1500
+         // = 1200 / 1500 = 4 / 5
+         expected_margin_call_pays_ratio = ratio_type(4, 5);
+         BOOST_CHECK(expected_margin_call_pays_ratio == feed.margin_call_pays_ratio(mcfr));
+
+
+         //////
+         // Check the fee when MSSR = 150% and MCFR = 60%
+         //////
+         mcfr = 600; // 60% expressed in terms of GRAPHENE_COLLATERAL_RATIO_DENOM
+
+         // Expected paid price = price / MSSR
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (1500 / 1000)
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core) / (3 / 2)
+         // = (1 satoshi SMARTBIT2 / 30 satoshi Core)
+         expected_paid_price = price(smartbit2.amount(1), core.amount(30));
+         BOOST_CHECK(expected_paid_price == feed.max_short_squeeze_price());
+
+         // Expected offer price = price / (MSSR-MCFR)
+         // but (MSSR-MCFR) has a floor 1
+         // Therefore = price / 1 = price
+         // = (1 satoshi SMARTBIT2 / 20 satoshi Core)
+         expected_paid_price = price(smartbit2.amount(1), core.amount(20));
+         BOOST_CHECK(expected_paid_price == feed.margin_call_order_price(mcfr));
+
+         // Expected margin call pays ratio = (MSSR-MCFR) / MSSR
+         // but (MSSR-MCFR) has a floor 1
+         // Therefore = 1 / MSSR
+         // = 1000 / 1500 = 2 / 3
+         expected_margin_call_pays_ratio = ratio_type(2, 3);
+         BOOST_CHECK(expected_margin_call_pays_ratio == feed.margin_call_pays_ratio(mcfr));
 
       }
       FC_LOG_AND_RETHROW()
